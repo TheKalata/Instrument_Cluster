c1eb1e4c20bc63ff259b90ac707cb0f2cd219e05
TheKalata <84922303+TheKalata@users.noreply.github.com>
2025-03-02 17:04:44 +0200

    mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,13 +10,11 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     this->setStyleSheet("background-color: black;");
-    this->setFixedSize(953, 408); //ratio = 21:9
+    this->setFixedSize(953, 408);
     this->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
-    // Създаване на инструменталния клъстер
     InstrumentCluster *cluster = new InstrumentCluster(this);
 
-    // Добавяне към QWidget от дизайнера (например clusterWidget)
     QVBoxLayout *layout = new QVBoxLayout(ui->clusterWidget);
     layout->addWidget(cluster);
 }
